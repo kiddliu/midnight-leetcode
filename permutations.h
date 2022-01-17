9123c2dea166264c556c98b004072908eeff978c
Kidd Liu <kiddliu.sz@gmail.com>
2022-01-17 19:45:22 +0800

    feat(medium): Permutations II

--- permutations.h
+++ permutations.h
@@ -9,6 +9,7 @@ namespace solution {
 
 std::vector<std::vector<int>> permute(std::vector<int>& nums) {
   // since we've dealt with std::next_permutation...
+  // it's the same for permutation ii
   // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Permutations.
   // Memory Usage: 7.6 MB, less than 78.37% of C++ online submissions for Permutations.
   //
