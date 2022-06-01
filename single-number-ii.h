eb12344181d2371486c4035c06ff56e34e842ceb
Kidd Liu <kiddliu.sz@gmail.com>
2022-06-02 06:49:24 +0800

    feat(medium): Single Number II

---
+++ single-number-ii.h
@@ -0,0 +1,24 @@
+﻿#ifndef SINGLE_NUMBER_II_H_
+#define SINGLE_NUMBER_II_H_
+
+#include <vector>
+
+namespace solution {
+
+int singleNumber(std::vector<int>& nums) {
+  // 有必要复习一下数字电路了
+  // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Single Number II.
+  // Memory Usage: 9.5 MB, less than 87.36% of C++ online submissions for Single Number II.
+  //
+  auto first{0}, second {0};
+
+  for (auto& n : nums) {
+    first = (first ^ n) & ~second;
+    second = (second ^ n) & ~first;
+  }
+  return first;
+}
+
+}
+
+#endif  // SINGLE_NUMBER_II_H_
